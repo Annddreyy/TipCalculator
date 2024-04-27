50a178eba1f506945d7e1fa0fbe355cb84df88e6
Andrey22 <andreymail22112006@gmail.com>
2024-04-27 16:53:28 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/kotlin6/ExampleInstrumentedTest.kt
@@ -0,0 +1,40 @@
+package com.example.kotlin6
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.kotlin6.ui.theme.Kotlin6Theme
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+import java.text.NumberFormat
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun useAppContext() {
+        composeTestRule.setContent {
+            Kotlin6Theme {
+                MainScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill count").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip percentage").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists("No node with this text was found.")
+    }
+}
---
+++ app/src/test/java/com/example/kotlin6/ExampleUnitTest.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin6
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.text.NumberFormat
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
